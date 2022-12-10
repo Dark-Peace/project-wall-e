b4362658021586298e765302be5f06f3f4b66639
nobourge <57195536+nobourge@users.noreply.github.com>
2022-12-10 12:55:03 +0100

    every value plot loop

--- WxE.m
+++ WxE.m
@@ -3,7 +3,7 @@
 y0 = [-2,2];
 %y0 = [-1,1];
 %y0 = [-1,1];
-%y0 = [0,2]; %41
+%y0 = [0,2]; %41 bad
 tspan = [0,10];
 a = -0.15;
 b = 0.9;
@@ -22,10 +22,30 @@ A3 = [a b;
 A4 = [a 0;
       b 0];
 
+%coefficient_matrices = [A1,A2,A3,A4];
+coefficient_matrices = {A1,A2,A3,A4};
+
+%disp('coefficient_matrices size');
+%disp('coefficient_matrices size'+numel(coefficient_matrices));
+fprintf('coefficient_matrices numel : %d .\n',numel(coefficient_matrices));
+fprintf('coefficient_matrices length : %d .\n',length(coefficient_matrices));
+
+%for a = 1:numel(coefficient_matrices) % numel(coefficient_matrices) = 16
+for a = 1:length(coefficient_matrices)
+    fprintf('matrix : %d \n',a);
+
+    %A = coefficient_matrices{a};  %error: matrix cannot be indexed with {
+    A = coefficient_matrices{a};
+
+    %A = a;
+    disp(A);
+
+endfor
+
 # love equation function
 function [t,x] = system_simulation(A, tspan, y0)
-    ode_sys = @(t,x) [A(1,1)*x(1)+A(1,2)*x(2);A(2,1)*x(1)+A(2,2)*x(2)]; # Définition système
-    [t,x] = ode23(ode_sys, tspan, y0); # Résolution système
+    ode_sys = @(t,x) [A(1,1)*x(1)+A(1,2)*x(2);A(2,1)*x(1)+A(2,2)*x(2)]; # Definition system
+    [t,x] = ode23(ode_sys, tspan, y0); # Resolution system
 endfunction
 
 
@@ -82,56 +102,74 @@ function [x1,x2,x1p,x2p] = plot_portrait_phase_complete(A)
     legend("field","isocline_1","isocline_2","v_1","v_2","location","south","orientation", "horizontal");
 endfunction
 
+function system_simulation_plot(A,tspan,y0,figure_number,a,b)
+    [t,x] = system_simulation(A,tspan,y0);
+    figure(figure_number);
+    plot(t,x)
+    titl = ["Evolution de w(t) et e(t) pour t \in [0, 10] a=",num2str(a)," b=",num2str(b)];
+    title(titl)
+    xlabel("t")
+    ylabel("x")
+    legend("w(t)","e(t)","location","south","orientation", "horizontal");
+endfunction
+
+function system_simulation_and_portrait_phase_and_plot(tspan,coefficient_matrices,y0)
+    disp('system_simulation_and_portrait_phase_and_plot()')
+    figure_number = 11
+    listOfTuples = [[-0.15,0.9]
+    ,[-0.15,-0.9]
+    ,[0.15,0.9]
+    ,[0.15,-0.9]
+    ,[1,0.1]
+    ,[0.1,0.1]
+    ,[-0.1,-0.1]]
+
+     listOfTuples = {[-0.15,0.9]
+    ,[-0.15,-0.9]
+    ,[0.15,0.9]
+    ,[0.15,-0.9]
+    ,[1,0.1]
+    ,[0.1,0.1]
+    ,[-0.1,-0.1]}
+    %fprintf('listOfTuples size : %d .\n',numel(listOfTuples));
+    %fprintf('listOfTuples size/2 : %d .\n',numel(listOfTuples)/2);
+    fprintf('listOfTuples length : %d .\n',length(listOfTuples));
+
+    %for a = 1:numel(coefficient_matrices)/4
+    for a = 1:length(coefficient_matrices)
+        fprintf('coefficient_matrices length : %d .\n',length(coefficient_matrices));
+
+        A = coefficient_matrices{a};
+        %A = coefficient_matrices(a);
+        disp(A)
+
+        %for i = 1:numel(listOfTuples)/2 # from OpenAI GPT chat
+        for i = 1:length(listOfTuples)
+            % Get the current tuple
+            currentTuple = listOfTuples{i};
+            %currentTuple = listOfTuples(i);
+
+            % Extract the values from the tuple into two variables
+            a = currentTuple(1);
+            b = currentTuple(2);
+            %a = currentTuple{1};
+            %b = currentTuple{2};
+
+            % print the values
+            fprintf('a : %d .\n', a);
+            fprintf('b : %d .\n', b);
+
+            system_simulation_plot(A,tspan,y0,figure_number,a,b);
+            figure_number = figure_number + 1;
+            # display portrait de phases
+            %figure figure_number;
+            figure(figure_number);
+            [x1,x2,x1p,x2p] = plot_portrait_phase_complete(A);
+            figure_number = figure_number + 1;
+
+        endfor
+    endfor
+endfunction
 
 
-A = A1; # change Ax to plot another equation
-[t,x] = system_simulation(A, tspan, y0);
-figure 11
-plot(t,x)
-title("Evolution de w(t) et e(t) pour t \in [0, 10] ")
-xlabel("t")
-ylabel("x")
-legend("w(t)","e(t)","location","south","orientation", "horizontal");
-
-# display portrait de phases
-figure 12
-[x1,x2,x1p,x2p] = plot_portrait_phase_complete(A);
-
-A = A2; # change Ax to plot another equation
-[t,x] = system_simulation(A, tspan, y0);
-figure 21
-plot(t,x)
-title("Evolution de w(t) et e(t) pour t \in [0, 10] ")
-xlabel("t")
-ylabel("x")
-legend("w(t)","e(t)","location","south","orientation", "horizontal");
-
-
-figure 22
-[x1,x2,x1p,x2p] = plot_portrait_phase_complete(A);
-
-A = A3; # change Ax to plot another equation
-[t,x] = system_simulation(A, tspan, y0);
-figure 31
-plot(t,x)
-title("Evolution de w(t) et e(t) pour t \in [0, 10] ")
-xlabel("t")
-ylabel("x")
-legend("w(t)","e(t)","location","south","orientation", "horizontal");
-
-
-figure 32
-[x1,x2,x1p,x2p] = plot_portrait_phase_complete(A);
-
-A = A4; # change Ax to plot another equation
-[t,x] = system_simulation(A, tspan, y0);
-figure 41
-plot(t,x)
-title("Evolution de w(t) et e(t) pour t \in [0, 10] ")
-xlabel("t")
-ylabel("x")
-legend("w(t)","e(t)","location","south","orientation", "horizontal");
-
-
-figure 42
-[x1,x2,x1p,x2p] = plot_portrait_phase_complete(A);
+system_simulation_and_portrait_phase_and_plot(tspan,coefficient_matrices,y0)
